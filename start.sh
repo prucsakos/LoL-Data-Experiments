1cbaa0f1af49bf8de073985cf6384a2a52e09671
Ákos Prucs <prucsakos@gmail.com>
2024-07-15 02:41:02 +0200

    Patch 5

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Start the Python program with different arguments
-nohup python ./src/data-collector-2/main.py americas &
-nohup python ./src/data-collector-2/main.py europe &
-nohup python ./src/data-collector-2/main.py asia &
-nohup python ./src/data-collector-2/main.py sea &
+# Start the Python program with different arguments in the background
+python ./src/data-collector-2/main.py americas &
+python ./src/data-collector-2/main.py europe &
+python ./src/data-collector-2/main.py asia &
+python ./src/data-collector-2/main.py sea &
 
-# Wait indefinitely to keep the container running
-tail -f /dev/null
+# Wait for all background processes to finish
+wait
